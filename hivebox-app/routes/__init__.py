1f77ab55c39d2fe752319b45701b97432bbda27b
Boris M. <155096757+chfrchko5@users.noreply.github.com>
2025-10-13 13:26:53 -0400

    github actions workflow stuff (#11)
    * pip package for auto increment version
    
    * requirements change
    
    * requirements mismatch fix
    
    * last req fix
    
    * added python auto versioning
    
    * github actions file test
    
    * test
    
    * check
    
    * test2
    
    * test3
    
    * test4
    
    * finalizing auto tag feature
    
    * test for pytest
    
    * test
    
    * changes to unit tests
    
    * pytest test again
    
    * change to prod

--- hivebox-app/routes/__init__.py
+++ hivebox-app/routes/__init__.py
@@ -5,7 +5,6 @@ from fastapi import FastAPI, APIRouter
 app = FastAPI()
 router = APIRouter()
 
-# simple version endpoint, uses function print_version to display current app version
 @router.get("/version")
 async def appversion():
     return {"version": version.get_version()}
