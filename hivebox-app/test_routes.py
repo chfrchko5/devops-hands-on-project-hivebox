b3639029bc0a31b0d76258507a868dbd9cc20942
chfrchko5 <r4ger7@gmail.com>
2025-10-13 13:18:16 -0400

    changes to unit tests

--- hivebox-app/test_routes.py
+++ hivebox-app/test_routes.py
@@ -1,13 +1,16 @@
 from fastapi.testclient import TestClient
 from routes import app
+from version import get_version
+from api_data import average_temps
 
 client = TestClient(app)
 
 def test_get_version():
     response = client.get('/version')
     assert response.status_code == 200
-    assert response.json() == {"version": "v0.0.1"}
+    assert response.json() == {"version": get_version()}
 
 def test_get_temperature():
     response = client.get('/temperature')
     assert response.status_code == 200
+    assert response.json() == {"average temperature": average_temps}
