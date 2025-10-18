b5837e57497e0885d249c0bfa61cc74880be6140
chfrchko5 <r4ger7@gmail.com>
2025-10-18 13:10:12 -0400

    additional temperature information based on calculation

--- hivebox-app/routes/__init__.py
+++ hivebox-app/routes/__init__.py
@@ -11,6 +11,13 @@ async def appversion():
 
 @router.get("/temperature")
 async def temperature():
-    return {"average temperature": api_data.average_temps}
-
+    if api_data.average_temps <= 10:
+        return {"average temperature": f"{api_data.average_temps} (Too Cold)"}
+    elif 10 < api_data.average_temps <=36:
+        return {"average temperature": f"{api_data.average_temps} (Good)"}
+    elif api_data.average_temps > 36:
+        return {"average temperature": f"{api_data.average_temps} (Too Hot)"}
+    else:
+        return {"warning": "error in temperature calculation"}
+    
 app.include_router(router)
