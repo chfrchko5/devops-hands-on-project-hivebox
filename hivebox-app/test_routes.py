4a545a56574ab4950daa19068f12ed41f13c09d3
chfrchko5 <r4ger7@gmail.com>
2025-10-27 15:32:30 -0400

    test

--- hivebox-app/test_routes.py
+++ hivebox-app/test_routes.py
@@ -10,9 +10,9 @@ def test_get_version():
     assert response.status_code == 200
     assert response.json() == {"version": get_version()}
 
-def test_get_temperature():
-    response = client.get('/temperature')
-    value = response.json()
-    temp = value["average temperature"].split()[0]
-    assert response.status_code == 200
-    assert str(temp) == str(round(average_temps, 2))
+# def test_get_temperature():
+#     response = client.get('/temperature')
+#     value = response.json()
+#     temp = value["average temperature"].split()[0]
+#     assert response.status_code == 200
+#     assert str(temp) == str(round(average_temps, 2))
