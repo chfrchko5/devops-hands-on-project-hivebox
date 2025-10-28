94b4d036d22b29a491ca66182c6c0f25a3a5f9fb
chfrchko5 <r4ger7@gmail.com>
2025-10-28 11:21:38 -0400

    simple metric extension

--- hivebox-app/routes/__init__.py
+++ hivebox-app/routes/__init__.py
@@ -2,6 +2,7 @@ import version
 import api_data
 from fastapi import FastAPI, APIRouter, HTTPException
 from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_client import Counter
 import redis
 import os
 
@@ -19,6 +20,9 @@ app = FastAPI()
 router = APIRouter()
 
 Instrumentator().instrument(app).expose(app)
+
+cache_hits = Counter("cache_hits_total", "Number of successful cache hits")
+cache_misses = Counter("cache_misses_total", "Number of cache misses")
 
 CACHE_KEY_TEMPERATURE = "average_temperature"
 CACHE_TTL_SECONDS = 60
@@ -41,8 +45,10 @@ async def appversion():
 async def temperature():
     cached_value = valkey.get(CACHE_KEY_TEMPERATURE)
     if cached_value is not None:
+        cache_hits.inc()
         return {"average temperature": cached_value, "cached": True}
     
+    cache_misses.inc()
     try:
         avg = float(api_data.average_temps)
     except Exception as e:
