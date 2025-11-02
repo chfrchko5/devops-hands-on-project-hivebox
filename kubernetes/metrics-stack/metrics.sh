6a7b80d45f78bba1b0cdf0336b277ba53420b01e
chfrchko5 <r4ger7@gmail.com>
2025-11-02 14:46:46 -0500

    grafana dashboards and metrics

--- kubernetes/metrics-stack/metrics.sh
+++ kubernetes/metrics-stack/metrics.sh
@@ -1,3 +1,10 @@
-# like in the nginx ingress script
-# get the helm chart for kube prometheus stack
-# using the chart version and chart version
+#!/bin/bash
+
+CHART_VERSION="79.1.0"
+APP_VERSION="v0.86.1"
+
+helm template metrics kube-prometheus-stack \
+--repo https://prometheus-community.github.io/helm-charts \
+--version ${CHART_VERSION} \
+--namespace metrics \
+> metrics.${APP_VERSION}.yaml
--- kubernetes/nginx-ingress/nginx-ingress.sh
+++ kubernetes/nginx-ingress/nginx-ingress.sh
@@ -3,10 +3,8 @@
 CHART_VERSION="4.13.3"
 APP_VERSION="1.13.3"
 
-mkdir ./kubernetes/ingress/controller/nginx/manifests/
-
 helm template ingress-nginx ingress-nginx \
 --repo https://kubernetes.github.io/ingress-nginx \
 --version ${CHART_VERSION} \
 --namespace ingress-nginx \
-> .nginx-ingress.${APP_VERSION}.yaml
+> nginx-ingress.${APP_VERSION}.yaml
